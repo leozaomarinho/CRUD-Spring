49ccb11b4a64736acc4eced6ec13fb8dc3ca5abe
leonardo <leowalkg6@Gmail.com>
2025-02-07 00:18:38 +0000

    creating User entity

---
+++ src/main/java/com/example/crud/model/User.java
@@ -0,0 +1,20 @@
+package com.example.crud.model;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity //sera uma tabela na db
+@Data
+public class User {
+    @Id //pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //auto increment
+    private Long id;
+    private String username;
+    private String password; //senha armazenada em texto puro, altera para armazenar em hash
+
+
+}
